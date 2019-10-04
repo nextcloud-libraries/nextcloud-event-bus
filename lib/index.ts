629253e6b37489897b3d4ad0fc715c93b9040aac
Christoph Wurst <christoph@winzerhof-wurst.at>
2019-10-04 11:44:26 +0200

    Do not rely on global OC
    Otherwise we can't use this lib in server where OC isn't initialized yet
    
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- lib/index.ts
+++ lib/index.ts
@@ -1,22 +1,29 @@
-import { EventBus } from "./EventBus";
-import { ProxyBus } from "./ProxyBus";
-import { SimpleBus } from "./SimpleBus";
+import { EventBus } from "./EventBus"
+import { ProxyBus } from "./ProxyBus"
+import { SimpleBus } from "./SimpleBus"
 
-declare var OC: any;
-
-function getBus(): EventBus {
-    // Either use an existing event bus instance or create one
-    if ((typeof OC !== 'undefined') && OC._eventBus) {
-        return new ProxyBus(OC._eventBus)
-    } else if ((typeof OC !== 'undefined')) {
-        return OC._eventBus = new SimpleBus()
-    } else {
-        console.warn('no global OC found, creating a local event bus')
-        return new SimpleBus()
+declare global {
+    interface Window {
+        OC: any
+        _nc_event_bus: any
     }
 }
 
-const bus = getBus();
+function getBus(): EventBus {
+    if ((typeof window.OC !== 'undefined') && window.OC._eventBus && typeof window._nc_event_bus === 'undefined') {
+        console.warn('found old event bus instance at OC._eventBus. Update your version!')
+        window._nc_event_bus = window.OC._eventBus
+    }
+
+    // Either use an existing event bus instance or create one
+    if (typeof window._nc_event_bus !== 'undefined') {
+        return new ProxyBus(window._nc_event_bus)
+    } else {
+        return window._nc_event_bus = new SimpleBus()
+    }
+}
+
+const bus = getBus()
 
 export const subscribe = bus.subscribe.bind(bus)
 export const unsubscribe = bus.unsubscribe.bind(bus)
